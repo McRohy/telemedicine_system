9d722e79405c5fb80ccf95fbbcba922ed8e0946d
Matej Roháč <matej.rohac24@gmail.com>
2026-02-17 17:48:23 +0100

    initialize project structure

---
+++ backend/src/test/java/sk/uniza/fri/telemedicine/TelemedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.uniza.fri.telemedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
