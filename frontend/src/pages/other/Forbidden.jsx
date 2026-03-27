961dbfcfe21d4510d43897170267e04233ee12ed
Matej Roháč <matej.rohac24@gmail.com>
2026-03-27 11:37:53 +0100

    refactor[frontend]: improve Forbidden page layout with icon

--- frontend/src/pages/other/Forbidden.jsx
+++ frontend/src/pages/other/Forbidden.jsx
@@ -1,13 +1,18 @@
-import { Stack, Title, Text, Center } from "@mantine/core";
+import { Stack, Title, Text, Center, Group } from '@mantine/core';
+import { IconHandStop } from '@tabler/icons-react';
 
 export default function Forbidden() {
   return (
     <Center w="100vw" h="100vh" bg="primary">
-        <Stack gap="xl" align="center">
-           <Title order={1} c="black">403 Forbidden</Title>
-           <Text c="white">Nemáte povolenie na prístup k tejto stránke.</Text>
-        </Stack>
- 
+      <Stack gap="xl" align="center">
+        <Group>
+          <IconHandStop size={48} stroke={1} />
+          <Title order={1} c="black">
+            403 Forbidden
+          </Title>
+        </Group>
+        <Text>Nemáte povolenie na prístup k tejto stránke.</Text>
+      </Stack>
     </Center>
   );
 }
